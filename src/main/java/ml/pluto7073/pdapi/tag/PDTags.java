078024243debff10cef5db77affbec8275fc2ffd
pluto7073 <20planetpluto05@gmail.com>
2024-03-30 23:32:27 -0400

    Added Milk Bottles to conventional tag for compat w/Farmers Delight

--- src/main/java/ml/pluto7073/pdapi/tag/PDTags.java
+++ src/main/java/ml/pluto7073/pdapi/tag/PDTags.java
@@ -4,9 +4,11 @@ import ml.pluto7073.pdapi.PDAPI;
 import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 
 public class PDTags {
 
     public static final TagKey<Item> WORKSTATION_DRINKS = TagKey.of(RegistryKeys.ITEM, PDAPI.asId("workstation_drinks"));
+    public static final TagKey<Item> MILK_BOTTLES = TagKey.of(RegistryKeys.ITEM, new Identifier("c:milk/milk_bottle"));
 
 }
