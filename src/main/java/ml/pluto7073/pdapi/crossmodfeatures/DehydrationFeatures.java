4cf4b38ad226dfdaeeb1464b21c9b19ba7f355dd
pluto7073 <20planetpluto05@gmail.com>
2024-07-25 16:23:20 -0400

    Prepared for 1.20.2 (dehydration don't support that version)

--- src/main/java/ml/pluto7073/pdapi/crossmodfeatures/DehydrationFeatures.java
+++ src/main/java/ml/pluto7073/pdapi/crossmodfeatures/DehydrationFeatures.java
@@ -1,14 +1,18 @@
 package ml.pluto7073.pdapi.crossmodfeatures;
 
-import net.dehydration.init.EffectInit;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 
 public class DehydrationFeatures {
 
     static void addThirstEffect(LivingEntity user, int duration, int amplifier, boolean onlyIfNonExistent) {
-        if (user.hasEffect(EffectInit.THIRST)) return;
-        user.addEffect(new MobEffectInstance(EffectInit.THIRST, duration, amplifier));
+        MobEffect thirst = BuiltInRegistries.MOB_EFFECT.get(new ResourceLocation("dehydration:thirst_effect"));
+        if (thirst == null) throw new IllegalStateException("How did this happen? (Apparently the mod \"dehydration\" isn't actually installed");
+        if (user.hasEffect(thirst)) return;
+        user.addEffect(new MobEffectInstance(thirst, duration, amplifier));
     }
 
 }
