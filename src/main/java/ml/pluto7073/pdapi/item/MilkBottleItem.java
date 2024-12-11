4e689d367b4c596cfe04515b16470e47d067efef
pluto7073 <20planetpluto05@gmail.com>
2024-12-11 14:33:56 -0500

    Removing unused things and code cleanup

--- src/main/java/ml/pluto7073/pdapi/item/MilkBottleItem.java
+++ src/main/java/ml/pluto7073/pdapi/item/MilkBottleItem.java
@@ -1,5 +1,6 @@
 package ml.pluto7073.pdapi.item;
 
+import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
@@ -17,6 +18,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 
+@MethodsReturnNonnullByDefault
 public class MilkBottleItem extends Item {
 
     private static final FoodProperties MilkFood = (new FoodProperties.Builder()).alwaysEat().nutrition(1).saturationMod(5).build();
