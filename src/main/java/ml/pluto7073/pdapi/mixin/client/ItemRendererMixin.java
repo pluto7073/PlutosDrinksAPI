ed3dd342f6836e3e21fa67ab383f3a19fdbf0350
pluto7073 <20planetpluto05@gmail.com>
2025-01-02 15:15:02 -0500

    Specialty Drinks are now rendered as their base items

---
+++ src/main/java/ml/pluto7073/pdapi/mixin/client/ItemRendererMixin.java
@@ -0,0 +1,37 @@
+package ml.pluto7073.pdapi.mixin.client;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.mojang.blaze3d.vertex.PoseStack;
+import ml.pluto7073.pdapi.item.PDItems;
+import ml.pluto7073.pdapi.specialty.SpecialtyDrink;
+import ml.pluto7073.pdapi.util.DrinkUtil;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(ItemRenderer.class)
+public abstract class ItemRendererMixin {
+
+    @Shadow public abstract BakedModel getModel(ItemStack stack, @Nullable Level world, @Nullable LivingEntity entity, int seed);
+
+    @WrapMethod(method = "render")
+    private void pdapi$ReplaceModelAndStack(ItemStack stack, ItemDisplayContext modelTransformationMode, boolean leftHanded, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay, BakedModel model, Operation<Void> original) {
+        if (!stack.is(PDItems.SPECIALTY_DRINK)) {
+            original.call(stack, modelTransformationMode, leftHanded, matrices, vertexConsumers, light, overlay, model);
+        } else {
+            SpecialtyDrink drink = DrinkUtil.getSpecialDrink(stack);
+            ItemStack newStack = drink.getBaseItem(stack);
+            BakedModel baseDrinkModel = getModel(newStack, null, null, 0);
+            original.call(drink.color() == -1 ? newStack : stack, modelTransformationMode, leftHanded, matrices, vertexConsumers, light, overlay, baseDrinkModel);
+        }
+    }
+
+}
